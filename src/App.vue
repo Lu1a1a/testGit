1b0af8cd02fbd0da3ef33974d009d4537c09c110
lulala <czps40511@gmail.com>
2025-03-13 11:22:38 +0800

    updata

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import Home from "./components/home.vue";
-// import { ref } from "vue";
 </script>
 <template>
-  <Home name="shao" :id="1" />
+  <h1>123123</h1>
 </template>
 
 <style scoped></style>
